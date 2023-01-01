71946c71b69572965e7a5eea361f6af422545869
rimorgin <calvscalderonx@gmail.com>
2023-01-01 08:07:14 +0800

    feat: added defects

--- another_test.py
+++ another_test.py
@@ -49,7 +49,7 @@ img = removeBG(args['image'], output_filename)
 
 # Convert the image to grayscale and blur it slightly
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (7, 7), 0)
+gray = cv2.GaussianBlur(gray, (11, 11), 0)
 
 # Perform edge detection
 edged = cv2.Canny(gray, 50, 100)
@@ -69,26 +69,92 @@ if len(cnts) == 0:
     print("No contours found.")
 else:
     # Get the contour with the max area (largest object)
-    c = max(cnts, key=cv2.contourArea)
+    c = max(cnts, key = lambda x: cv2.contourArea(x))
 
-    # Approximate the contour to reduce its complexity
-    epsilon = 0.01 * cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, epsilon, True)
-
-    # Get the convex hull of the simplified contour
-    hull = cv2.convexHull(approx)
+    hull = cv2.convexHull(c)
+    hull_indices = cv2.convexHull(c, returnPoints=False)
     
-
+    #approx the contour a little
+    epsilon = 0.0005 * cv2.arcLength (c,True)
+    approx= cv2.approxPolyDP(c,epsilon,True)
+    
+    #make convex hull around hand
+    hull = cv2.convexHull(c)
+    
+    #define area of hull and area of hand
+    areahull = cv2.contourArea(c)
+    areacnt = cv2.contourArea(c)
+    
+    #find the percentage of area not covered by hand in convex hull
+    arearatio=((areahull-areacnt)/areacnt)*100
+    
     # Draw the original contour and the convex hull on the image
     cv2.drawContours(img, [c], -1, (0, 255, 0), 2)  # Original contour in green
     cv2.drawContours(img, [hull], -1, (255, 0, 0), 2)  # Convex hull in blue
+    
+    cv2.imshow('Convex Hull', img)
+    
+    hull = cv2.convexHull(approx, returnPoints=False)
+    hull[::-1].sort(axis=0)
+    defects = cv2.convexityDefects(approx, hull)
+    
+    # Check if convexity defects are present
+    if defects is not None:
+        defects = defects[:, 0, :]  # Reshape defects
+        
+        # Sort defects by depth (distance between the farthest point and the convex hull)
+        sorted_defects = defects[np.argsort(defects[:, 3]), :]
+        
+        # Keep only the largest defects (likely corresponding to finger segments)
+        # Here, we are assuming that the top 5 largest defects correspond to fingers
+        finger_defects = sorted_defects[-5:]  # Get 5 largest defects
+        
+        start_points, end_points, far_points = [], [], []
+        
+        for i in range(len(finger_defects)):
+            s, e, f, d = finger_defects[i]
+            start_points.append(tuple(approx[s][0]))  # Start of the defect
+            end_points.append(tuple(approx[e][0]))    # End of the defect
+            far_points.append(tuple(approx[f][0]))    # Farthest point of the defect
 
-    cv2.imshow("Convexity Hull", img)
-   
+            # Draw the defect
+            #cv2.line(img, start_points[-1], end_points[-1], (0, 255, 0), 2)  # Line between start and end
+            cv2.circle(img, far_points[-1], 5, (0, 0, 255), -1)              # Defect point
+
+        # Convert lists to arrays for easier manipulation
+        far_points = np.array(far_points)
+
+        # Step 1: Sort by the Y-coordinate (to find the top-most points first)
+        sorted_indices = np.argsort(far_points[:, 1])
+        far_points = far_points[sorted_indices]
+        start_points = np.array(start_points)[sorted_indices]
+        end_points = np.array(end_points)[sorted_indices]
+
+        # Step 2: Calculate a threshold to eliminate wrist points
+        average_finger_height = np.mean(far_points[:, 1])
+        threshold = average_finger_height + (0.2 * (far_points[:, 1].max() - far_points[:, 1].min()))
+
+        # Filter points above the threshold (likely fingers, not the wrist)
+        valid_indices = np.where(far_points[:, 1] < threshold)[0]
+        
+        # Use only the valid points
+        far_points = far_points[valid_indices]
+        start_points = start_points[valid_indices]
+        end_points = end_points[valid_indices]
+
+        # Draw filtered points
+        for pt in far_points:
+            cv2.circle(img, tuple(pt), 5, (255, 0, 0), -1)  # Highlight valid points (in blue)
+
+        # Show the final image with convexity defects
+        cv2.imshow("Convexity Defects", img)
+    else:
+        print("No convexity defects found.")
+
 
 
     # Show the final image with the original contour, convex hull, and convexity defects
-    #cv2.imshow("Convexity Defects", img)
+    
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
