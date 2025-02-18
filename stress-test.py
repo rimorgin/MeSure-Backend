bad2a3fc8c9a4122e0507c47cc55881279f96477
rimorgin <calvscalderonx@gmail.com>
2025-02-18 18:24:59 +0800

    feat: update stress test script to track completed and failed requests

--- stress-test.py
+++ stress-test.py
@@ -3,14 +3,23 @@ import threading
 import time
 
 # replace url with local or prod servers
-url = 'http://localhost:8080/measure-fingers'
+url = 'https://python.jaitolentino.studio/measure-fingers'
 
+completed_requests = 0
+failed_requests = 0
 
 def send_request():
+    global completed_requests
+    global failed_requests
     try: 
-        response = requests.post(url, files={'image': open('images/palmdown-left.jpg', 'rb')}, data={'width': '20.5'})
-        #if response.status_code == 200: 
-        #    print(response.json())
+        response = requests.post(url, files={'image': open('images/palmdown-left.jpg', 'rb')}, data={'width': '20.5'}, verify=None)
+        if response.status_code != 200: 
+            failed_requests += 1
+            return
+        
+        print(response.json())
+        completed_requests += 1
+            
     except Exception as e:
         print("An error happened ->", e)
 
@@ -30,3 +39,5 @@ end_time = time.time()
 elapsed_time = end_time - start_time
 
 print(f"Elapsed time: {elapsed_time} seconds")
+print(f"Completed Requests: {completed_requests}")
+print(f"Failed Requests: {failed_requests}")
